2e9437dbe4d2e53c81cc36cdabc8f688f28d03d2
Revlos <revlos@protonmail.com>
2018-05-22 23:55:51 +0300

    - Made filling of the distribution folder.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require("gulp");
 // Require the browser-sync
 var browserSync = require("browser-sync").create();
+var del = require("del");
+var fs = require("fs");
+var runSequence = require("run-sequence");
 
 // Task to run a server thru browser-sync
 gulp.task("start", function() {
@@ -11,4 +14,34 @@ gulp.task("start", function() {
     browser: "google chrome",
     open: false
   });
+});
+
+/*Working with "distribution" folder:*/
+
+//--Copy some files and folders
+gulp.task("dist-fill-copy", function (cb) {
+  gulp.src("production/**/*")
+  .pipe(gulp.dest("distribution"))
+  .on("finish", function() {
+    cb();
+  });
+
+});
+
+//--Delete some files
+gulp.task("dist-fill-delete", function (cb) {
+
+  del.sync(["*/*/", "!*/dist/"], {cwd: "distribution/bower_components", force: true});
+  cb();
+});
+
+//-Fill in dist folder
+gulp.task("dist-fill", function (cb) {
+  runSequence("dist-fill-copy", "dist-fill-delete", cb);
+});
+
+//-Empty dist folder
+gulp.task("dist-empty", function () {
+  //Delete all files
+  del.sync(["*"], {cwd: "distribution", force: true});
 });
